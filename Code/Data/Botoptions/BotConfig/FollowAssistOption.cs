a3e37faeff0060ef70d66e15e58cf4ece4d7f006
Xackery <xackery@gmail.com>
2015-09-12 14:16:00 -0700

    Moved BotConfig into it's own subdirectory, added 2 appended class types to organize the options cleaner.

---
+++ Code/Data/Botoptions/BotConfig/FollowAssistOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L2_login
+{
+    class FollowAssistOption
+    {
+        public int IsEnabled = 0;
+        public string Target = "";
+        public int Distance = 10;
+        public int AssistAttackWhenHPLessThan = 0;
+        public int AssistTarget = 0;
+    }
+}
---
+++ Code/Data/Botoptions/BotConfig/FollowOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L2_login
+{
+    class FollowOption
+    {
+        public volatile int WalkerStyle = 0;
+        public volatile FollowAssistOption LogicWhenIdle = new FollowAssistOption();
+        public volatile FollowAssistOption LogicWhenCombat = new FollowAssistOption();
+    }
+}
