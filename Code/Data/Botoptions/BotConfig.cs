b91c8f98140dc654e2a13ee605698c605318dd69
Xackery <xackery@gmail.com>
2015-09-12 00:42:27 -0700

    Added my initial copy of Botconfig/FollowOption

---
+++ Code/Data/Botoptions/BotConfig.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace L2_login
+{
+    /// <summary>
+    /// Bot Configuration for loading/saving from new flat file system, replaces the old BotOptions
+    /// </summary>
+    class BotConfig
+    {
+        //public volatile FollowOption Follow = new FollowOption();
+
+        private static volatile BotConfig instance;
+        /// <summary>
+        /// BotConfig is a singleton instance, so call this to get the current instance settings.
+        /// </summary>
+        /// <returns></returns>
+        public static BotConfig Instance()
+        {
+            if (instance == null)
+            {
+                instance = new BotConfig();
+            }
+            return instance;
+        }
+
+        public static void Save(string path)
+        {
+            if (instance == null)
+            {
+                instance = new BotConfig();
+            }
+
+            JsonTextWriter writer = new JsonTextWriter(new StreamWriter(path));
+
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(writer, instance);
+            writer.Close();
+
+        }
+        public static void Load(string path)
+        {
+            
+            JsonTextReader reader = new JsonTextReader(new StreamReader(path));
+
+            JsonSerializer serializer = new JsonSerializer();
+            instance = serializer.Deserialize<BotConfig>(reader);
+            reader.Close();
+        }
+        
+
+        public static void Clear()
+        {
+            instance = new BotConfig();
+        }
+        
+    }
+}
---
+++ Code/Data/Botoptions/FollowOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L2_login
+{
+    class FollowOption
+    {
+        public volatile bool WhenIdle = false;
+        public volatile bool Always = false;
+
+    }
+}
